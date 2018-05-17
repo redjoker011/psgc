d12ff82dd39af73d0e2b52a89958b682a5139c02
Peter John Alvarado <redjoker011@gmail.com>
2018-05-17 14:19:05 +0800

    Add data for provinces and municipalities

--- lib/psgc.rb
+++ lib/psgc.rb
@@ -1,5 +1,8 @@
 require "psgc/version"
 
 module Psgc
-  # Your code goes here...
+  provinces = File.join(File.dirname(__FILE__), "data", "provinces.json")
+  cities    = File.join(File.dirname(__FILE__), "data", "cities_municipalities.json")
+  PROVINCE_DATA = JSON.parse(File.read(provinces))
+  CITIES_DATA = JSON.parse(File.read(cities))
 end
