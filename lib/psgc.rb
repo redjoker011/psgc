e9c068d37dde8285b1ca4d789c24fb9325533aa5
Peter John Alvarado <redjoker011@gmail.com>
2018-05-17 15:12:24 +0800

    Update Regions keys and methods for fetching regions and provinces

--- lib/psgc.rb
+++ lib/psgc.rb
@@ -2,17 +2,25 @@ require "psgc/version"
 require "json"
 
 module Psgc
-  provinces = File.join(File.dirname(__FILE__), "data", "provinces.json")
-  cities    = File.join(File.dirname(__FILE__), "data", "cities_municipalities.json")
-  PROVINCE_DATA = JSON.parse(File.read(provinces))
-  CITIES_DATA = JSON.parse(File.read(cities))
+  regions_data   = File.join(File.dirname(__FILE__), "data", "regions.json")
+  provinces_data = File.join(File.dirname(__FILE__), "data", "provinces.json")
+  cities_data    = File.join(File.dirname(__FILE__), "data", "cities_municipalities.json")
 
-  def self.provinces
-    PROVINCE_DATA["data"]
+  REGIONS   = JSON.parse(File.read(regions_data))
+  PROVINCES = JSON.parse(File.read(provinces_data))
+  CITIES    = JSON.parse(File.read(cities_data))
+
+  def self.regions
+    REGIONS["data"]
+  end
+
+  def self.provinces(region_code:)
+    provinces = PROVINCES["data"]
+    provinces.select{ |data| data["region_code"] == region_code.to_s }
   end
 
   def self.cities(province_code:)
-    cities = CITIES_DATA["data"]
+    cities = CITIES["data"]
     cities.select{ |data| data["province_code"] == province_code.to_s }
   end
 end
