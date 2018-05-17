6b1f403b0355bdd33c973ac3b3d4697597bd4777
Peter John Alvarado <redjoker011@gmail.com>
2018-05-17 14:49:05 +0800

    Create Method for fetching cities

--- lib/psgc.rb
+++ lib/psgc.rb
@@ -6,4 +6,9 @@ module Psgc
   cities    = File.join(File.dirname(__FILE__), "data", "cities_municipalities.json")
   PROVINCE_DATA = JSON.parse(File.read(provinces))
   CITIES_DATA = JSON.parse(File.read(cities))
+
+  def self.cities(province_code:)
+    cities = CITIES_DATA["data"]
+    cities.select{ |data| data["province_code"] == province_code.to_s }
+  end
 end
