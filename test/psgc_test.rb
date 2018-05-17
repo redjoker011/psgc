c7b861fd6608ec4aaf057383f5a962cba2ccbfa6
Peter John Alvarado <redjoker011@gmail.com>
2018-05-17 20:21:01 +0800

    Create test for .regions

--- test/psgc_test.rb
+++ test/psgc_test.rb
@@ -5,7 +5,30 @@ class PsgcTest < Minitest::Test
     refute_nil ::Psgc::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  describe ".regions" do
+    before { @regions = Psgc.regions }
+    it "returns regions" do
+      refute @regions.empty?
+    end
+    it "returns 17 regions" do
+      # [ 1] "REGION I (ILOCOS REGION)",
+      # [ 2] "REGION II (CAGAYAN VALLEY)",
+      # [ 3] "REGION III (CENTRAL LUZON)",
+      # [ 4] "REGION IV-A (CALABARZON)",
+      # [ 5] "REGION IV-B (MIMAROPA)",
+      # [ 6] "REGION V (BICOL REGION)",
+      # [ 7] "REGION VI (WESTERN VISAYAS)",
+      # [ 8] "REGION VII (CENTRAL VISAYAS)",
+      # [ 9] "REGION VIII (EASTERN VISAYAS)",
+      # [10] "REGION IX (ZAMBOANGA PENINSULA)",
+      # [11] "REGION X (NORTHERN MINDANAO)",
+      # [12] "REGION XI (DAVAO REGION)",
+      # [13] "REGION XII (SOCCSKSARGEN)",
+      # [14] "NATIONAL CAPITAL REGION (NCR)",
+      # [15] "CORDILLERA ADMINISTRATIVE REGION (CAR)",
+      # [16] "AUTONOMOUS REGION IN MUSLIM MINDANAO (ARMM)",
+      # [17] "REGION XIII (Caraga)"
+      assert_equal @regions.count, 17
+    end
   end
 end
